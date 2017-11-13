e6d1d44d6975a7d7445cc59e2e66b2b395ecbf8e
johnemmanuelgaa <32560332+johnemmanuelgaa@users.noreply.github.com>
2017-11-13 18:54:46 +0800

    mobile friendly website LCBBR

---
+++ mail/contact_me.php
@@ -0,0 +1,57 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "No arguments Provided!";
+   return false;
+   }
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+switch ( $_POST['branch'] ) {
+    case "trece":
+        $to = 'johnemmanuelgaa@gmail.com';
+        break;
+    case "lancaster":
+        $to = 'johnemmanuelgaa@gmail.com';
+        break;
+    case "molino":
+        $to = 'johnemmanuelgaa@gmail.com';
+        break;
+    case "bacoor":
+        $to = 'johnemmanuelgaa@gmail.com';
+        break;
+    case "dasma":
+        $to = 'johnemmanuelgaa@gmail.com';
+        break;
+    case "manila":
+        $to = 'johnemmanuelgaa@gmail.com';
+        break;
+    case "lipa":
+        $to = 'johnemmanuelgaa@gmail.com';
+        break;
+    case "calamba":
+        $to = 'johnemmanuelgaa@gmail.com';
+        break;
+    case "tagaytay":
+        $to = 'johnemmanuelgaa@gmail.com';
+        break;
+    default:
+        $to = 'johnemmanuelgaa@gmail.com';
+        break;
+}
+
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: info@ladychristinestrece.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+return true;
+?>
